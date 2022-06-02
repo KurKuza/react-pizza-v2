d12b0ab54237872bef44488165093a857f1b7559
Kurkuza <danilsherrbakov1993@gmail.com>
2022-06-02 22:31:17 +0300

    adaptive

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './scss/app.scss'
 import Header from './components/Header'
 import Home from './pages/Home'
 import NotFound from './pages/NotFound'
-import Cart from './pages/Cart';
+import Cart from './pages/Cart'
 
 function App() {
 	return (
@@ -13,13 +13,11 @@ function App() {
 			<div className="wrapper">
 				<Header />
 				<div className="content">
-					<div className="container">
-						<Routes>
-							<Route path='/' element={<Home />} />
-							<Route path='/cart' element={<Cart />} />
-							<Route path='*' element={<NotFound />} />
-						</Routes>
-					</div>
+					<Routes>
+						<Route path='/' element={<Home />} />
+						<Route path='/cart' element={<Cart />} />
+						<Route path='*' element={<NotFound />} />
+					</Routes>
 				</div>
 			</div>
 		</div>

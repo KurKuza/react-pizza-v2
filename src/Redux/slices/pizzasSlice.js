fdbf12fd40f3261322d89f7594edda52dacf8993
Kurkuza <danilsherrbakov1993@gmail.com>
2022-06-22 22:43:31 +0300

    redux

---
+++ src/Redux/slices/pizzasSlice.js
@@ -0,0 +1,18 @@
+// import { createSlice } from '@reduxjs/toolkit'
+
+// const initialState = {
+// 	items: []
+// }
+
+
+// export const pizzasSlice = createSlice({
+// 	name: 'pizza',
+// 	initialState,
+// 	reducers: {
+// 		addItem(state, action) {}
+// 	},
+// })
+
+// export const {  } = pizzasSlice.actions
+
+// export default pizzasSlice.reducer
